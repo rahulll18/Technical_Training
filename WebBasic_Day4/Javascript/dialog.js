e38ed050a88f57e05a019deed27c3fff55444319
Rahul Avhad <rahulavhad005@student.sfit.ac.in>
2024-07-23 17:57:26 +0530

    day 4 over

---
+++ WebBasic_Day4/Javascript/dialog.js
@@ -0,0 +1,12 @@
+
+function fun()
+{
+    window.alert("Hurry Up !!")
+
+    const ans = window.confirm("Have you submitted the form ?")
+    console.log(ans);
+
+    const value = window.prompt("Enter your number:")
+    console.log(value)
+    console.log(typeof value)
+}
--- WebBasic_Day4/Javascript/function.js
+++ WebBasic_Day4/Javascript/function.js
@@ -15,3 +15,10 @@ let salaries = [50000,20000,30000];
 salaries.forEach(a => console.log(a));
 
 salaries.forEach((a , index) => console.log(a + " " + index));
+
+salaries.filter(salary => salary < 50000).forEach(salary => console.log(salary))
+
+salaries.map(salary => salary*2).forEach(salary => console.log(salary))
+
+var sum = salaries.reduce((accumulator , salary )=> accumulator + salary,0)
+console.log(sum)
