4eaaea314c61dfd6191d7560e34838aee0e6bd85
Rahul Avhad <rahulavhad005@student.sfit.ac.in>
2024-07-24 11:38:54 +0530

    Js day 2

---
+++ WebBasic_Day5/Stringdemo.js
@@ -0,0 +1,15 @@
+let companyName = "neoSoft";
+// console.log(companyName[3]);
+// console.log(companyName)
+// console.log(typeof companyName)
+
+let branch = new String("parel");
+console.log(branch)
+console.log(typeof branch)
+
+let company = "neoSoft";
+console.log(company === companyName);
+
+
+let companybranch = new String("parel");
+console.log(branch.valueOf() === companybranch.valueOf());
